243e9a96b3d689a7f6ed1740a9ba444af0d19957
Ani <annie.a.davtyan@gmail.com>
2025-02-04 13:53:53 +0400

    add: optimize search function with debouncing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,64 @@
 import logo from "../assets/logo.png"
-import { Link, useSearchParams } from "react-router-dom/dist";
+import { Link, useSearchParams } from "react-router-dom";
 import { LOGIN_URL, MAIN_URL } from "../helpers/urls";
 import { isAuth } from "../helpers/static";
 import { IoIosSearch } from "react-icons/io";
 import Dashboard from "./Dashboard";
+import { useState, useRef } from "react";
 
 function Header() {
-    const [searchParams, setSearchParams] = useSearchParams({})
+    const [searchParams, setSearchParams] = useSearchParams({});
+    const [query, setQuery] = useState("");
+    
+    // Use useRef to keep track of the timeout ID
+    const timeoutRef = useRef(null);
 
-    function searchProduct (value) {
-        setSearchParams(value !== "" ? { value } : "")
+    // Custom debounce function
+    function debounce(func, delay) {
+        return function (...args) {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+            timeoutRef.current = setTimeout(() => {
+                func(...args);
+            }, delay);
+        };
+    }
+
+    // Handle the search and update searchParams
+    const handleSearchInput = debounce((value) => {
+        setSearchParams(value !== "" ? { value } : {});
+    }, 500); // 500ms debounce delay
+
+    // Function to manage input changes
+    function onInputChange(e) {
+        const value = e.target.value;
+        setQuery(value);  // Update query state
+        handleSearchInput(value);  // Call debounced function
     }
 
     return (
-        <>
-            <div className="w-full flex flex-wrap justify-around p-5 items-center">
-                <Link to={MAIN_URL} className="w-[70px] h-[70px] rounded-md">
-                    <img src={logo} alt="Logo" />
-                </Link>
-                <div className="flex">
-                    <input className="rounded-3xl border-solid bg-slate-200 border-gray-200 w-[300px] px-[10px] py-[5px]"
-                        type="text"
-                        placeholder="Search"
-                        onChange={e => searchProduct(e.target.value)} 
-                    />
-                    <IoIosSearch className="w-[24px] h-[24px] relative top-[5px] right-[30px] rounded-[50%] text-[#424242]" />
-                </div>
-                {isAuth ?
-                    <>
-                        <Dashboard />
-                    </> :
-                    <>
-                        <Link to={LOGIN_URL}>Login</Link>
-                    </>}
+        <div className="w-full flex flex-wrap justify-around p-5 items-center">
+            <Link to={MAIN_URL} className="w-[70px] h-[70px] rounded-md">
+                <img src={logo} alt="Logo" />
+            </Link>
+            <div className="flex">
+                <input
+                    className="rounded-3xl border-solid bg-slate-200 border-gray-200 w-[300px] px-[10px] py-[5px]"
+                    type="text"
+                    placeholder="Search"
+                    value={query} // Bind input value to query state
+                    onChange={onInputChange}  // Update onChange to use our new function
+                />
+                <IoIosSearch className="w-[24px] h-[24px] relative top-[5px] right-[30px] rounded-[50%] text-[#424242]" />
             </div>
-        </>
-    )
+            {isAuth ? (
+                <Dashboard />
+            ) : (
+                <Link to={LOGIN_URL}>Login</Link>
+            )}
+        </div>
+    );
 }
 
-export default Header
+export default Header;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Category from "../components/Category";
 import FilteredProducts from "../components/FilteredProducts";
 import RangeSlidersComponent from "../components/RangeSlidersComponent";
@@ -9,58 +9,76 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts, changeRating } from "../redux/slice/ProductsSlice";
 
 function Products() {
-    const [show, setShow] = useState(false)
-    const [minPrice, setMinPrice] = useState()
-    const [maxPrice, setMaxPrice] = useState()
-    const [checked, setChecked] = useState(false)
-    const [searchParams, setSearchParams] = useSearchParams()
+    const [show, setShow] = useState(false);
+    const [minPrice, setMinPrice] = useState();
+    const [maxPrice, setMaxPrice] = useState();
+    const [checked, setChecked] = useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    const dispatch = useDispatch()
-    const { products, categories, price, rate } = useSelector(state => state.productsReducer)
+    const dispatch = useDispatch();
+    const { products, categories, price, rate } = useSelector((state) => state.productsReducer);
+
+    const filterRef = useRef(null); // Ref for filter dropdown and button
 
     useEffect(() => {
-        dispatch(getProducts())
-    }, [])
+        dispatch(getProducts());
+    }, []);
 
+    useEffect(() => {
+        // Click outside filter handler
+        function handleClickOutside(event) {
+            if (filterRef.current && !filterRef.current.contains(event.target)) {
+                setShow(false); // Close the dropdown
+            }
+        }
 
+        // Attach event listener
+        document.addEventListener("mousedown", handleClickOutside);
+
+        // Cleanup listener on unmount
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     function changePrice(min, max) {
-        setMinPrice(min)
-        setMaxPrice(max)
+        setMinPrice(min);
+        setMaxPrice(max);
     }
 
     function changeRate(newRate) {
-        dispatch(changeRating(newRate))
-        setChecked(false)
+        dispatch(changeRating(newRate));
+        setChecked(false);
     }
 
     function setFilters() {
-        setSearchParams(checked ? { minPrice, maxPrice } : { rate, minPrice, maxPrice })
-        setShow(false)
+        setSearchParams(checked ? { minPrice, maxPrice } : { rate, minPrice, maxPrice });
+        setShow(false);
     }
 
     function setAllRatings() {
-        setChecked(!checked)
-        dispatch(changeRating(0))
+        setChecked(!checked);
+        dispatch(changeRating(0));
     }
 
     return (
         <>
-            <div className='w-full flex flex-wrap justify-center relative'>
-                {
-                    price > 0 && <div className="flex align-center absolute start-0 top-[20px]">
+            <div className="w-full flex flex-wrap justify-center relative">
+                {price > 0 && (
+                    <div ref={filterRef} className="flex align-center absolute start-0 top-[20px]">
                         <button
                             className="flex justify-center align-center h-min mx-[20px] rounded-3xl border-solid bg-[#C70039] border-gray-200 w-[50px] px-[10px] py-[5px] text-white"
                             onClick={() => setShow(!show)}
                         >
                             {show ? <FaFilterCircleXmark /> : <FaFilter />}
                         </button>
-                        <div className={`flex flex-col gap-[30px] px-[20px] absolute w-min top-[20px] left-[80px] right-0 mb-[20px] z-10 border border-gray-300 rounded-md shadow-[#424242] shadow-lg p-[15px] bg-white h-[300px] ${show ? "" : "hidden"}`}>
+                        <div
+                            className={`flex flex-col gap-[30px] px-[20px] absolute w-min top-[20px] left-[80px] right-0 mb-[20px] z-10 border border-gray-300 rounded-md shadow-[#424242] shadow-lg p-[15px] bg-white h-[300px] ${
+                                show ? "" : "hidden"
+                            }`}
+                        >
                             <div className="font-bold text-[24px]">Filters</div>
-                            <RangeSlidersComponent
-                                price={price}
-                                changePrice={changePrice}
-                            />
+                            <RangeSlidersComponent price={price} changePrice={changePrice} />
 
                             <RatingComponent
                                 text="Filter by rate"
@@ -77,14 +95,12 @@ function Products() {
                             </button>
                         </div>
                     </div>
-                }
+                )}
                 {categories && <Category categories={categories} />}
             </div>
-            {products && <FilteredProducts
-                products={products}
-            />}
+            {products && <FilteredProducts products={products} />}
         </>
-    )
+    );
 }
 
-export default Products
+export default Products;
